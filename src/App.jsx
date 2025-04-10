0ab10fc70f5d5ffbda6ddd928b8ea5756d3ff987
acuraels <daniil.rib@mail.ru>
2025-04-10 14:15:31 +0500

    small-fix-commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from './pages/HomePage.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/yet-hr-platform-frontend/'>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<HomePage />} />
