17674ef268a058f6212c03f7cec332d92be35fef
acuraels <daniil.rib@mail.ru>
2025-06-15 14:14:59 +0500

    docker-file-add-and-axios-baseurl-fix

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://192.168.1.122:8000/api/v1/',
+    baseURL: 'http://127.0.0.1:8000/api/v1/',
     headers: {
         'Content-Type': 'application/json',
     }
